b6ebb1258a0b82542af0881280b1f7d78af16519
Pavel Korolev <dev@borodust.org>
2026-01-04 17:11:07 +0100

    Fix type in specializations API

--- resect.h
+++ resect.h
@@ -345,7 +345,7 @@ RESECT_API resect_bool resect_decl_is_template(resect_decl decl);
 
 RESECT_API resect_collection resect_decl_template_parameters(resect_decl decl);
 
-RESECT_API resect_collection resect_decl_template_specialization(resect_decl decl);
+RESECT_API resect_collection resect_decl_template_specializations(resect_decl decl);
 
 RESECT_API resect_type resect_decl_get_type(resect_decl decl);
 
